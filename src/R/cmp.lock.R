7a129b0dc2712d7d80dd50d60bfad385de4420e9
arekbee <arekbee@gmail.com>
2019-03-01 17:39:05 +0100

    Fixing feedback issues

--- src/R/cmp.lock.R
+++ src/R/cmp.lock.R
@@ -15,6 +15,16 @@ cmp.lock <- function(file1, file2) {
     same.row <- intersect(rownames(pdf1, pdf2))
     diff.row <- setdiff(rownames(pdf1, pdf2))
 
+    pnames1 <- unique(sapply(intersect(rownames(pdf1), diff.row), function(x) unlist(strsplit(x, split='.', fixed=TRUE))[1]))
+    pnames2 <- unique(sapply(intersect(rownames(pdf2), diff.row), function(x) unlist(strsplit(x, split='.', fixed=TRUE))[1]))
 
-    return(list(package.diff = diff.row))
+    same.row.pdf1 <- pdf1[same.row,]
+    same.row.pdf2 <- pdf2[same.row,]
+    
+    same.row.pd.diff <- same.row.pdf1 != same.row.pdf2 & !is.na(same.row.pdf1) & !is.na(same.row.pdf2)
+    res <- cbind(same.row.pdf1, same.row.pdf2)
+    rownames(res) <- same.row 
+    colnames(res) <- c('file1', 'file2')
+    
+    return(list(package.file1=pnames1, package.file2=pnames2, package.diff = res[same.row.pd.diff,]))
 }
